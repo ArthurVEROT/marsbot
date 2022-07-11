39f81795ba2a00e6e16df824818172b40815d85f
Arthur Vrt <46626919+ArthurVEROT@users.noreply.github.com>
2022-07-11 11:13:55 +0200

    logic move and change dir

--- index.js
+++ index.js
@@ -1,34 +1,7 @@
-const readline = require("readline");
-
-const lineReader = readline.createInterface({
-  input: require("fs").createReadStream(__dirname + "/instructions.txt"),
-});
-
-let lineCounter = 0;
-lineReader.on("line", (line) => {
-  lineCounter++;
-
-  printLine(line, lineCounter);
-});
-
-lineReader.on("close", () => {
-  console.log("We could now execute some final action");
-});
-
-const printLine = (line, i) => {
-  console.log(`Line ${i.toString().padStart(2, "0")}: ${line}`);
-};
-
-
 // make it move
 let x = "";
 let y = "";
 let cardinal = "";
-
-for (let i = 0; i < instructions.length; i++) {
-  move()
-}
-
 
 function forward(x, y, cardinal) {
   switch (cardinal) {
